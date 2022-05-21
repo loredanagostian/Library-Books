1522e91dc5801f2887eefe9ee4cd81904f1f548b
Lore <loredana.gostian@student.upt.ro>
2022-05-21 22:49:25 +0300

    encoded password

--- src/main/java/org/loose/fis/sre/services/userDB.java
+++ src/main/java/org/loose/fis/sre/services/userDB.java
@@ -6,13 +6,22 @@ import org.loose.fis.sre.controllers.dbConnection;
 import org.loose.fis.sre.exceptions.InvalidPassword;
 import org.loose.fis.sre.exceptions.UsernameAlreadyExistsException;
 import org.loose.fis.sre.exceptions.UsernameNotFound;
+<<<<<<< Updated upstream
 import org.loose.fis.sre.model.HistoryBook;
 import org.loose.fis.sre.model.User;
+=======
+<<<<<<< Updated upstream
+=======
+import org.loose.fis.sre.model.User;
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.sql.*;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 
 public class userDB {
@@ -32,7 +41,7 @@ public class userDB {
         preparedStatement = dbConnection.initiateConnection().prepareStatement(sql);
 
         preparedStatement.setString(1, username);
-        preparedStatement.setString(2, password);
+        preparedStatement.setString(2, encodePassword(username, password));
         preparedStatement.setString(3, role);
         preparedStatement.setString(4, name);
         preparedStatement.setString(5, email);
@@ -70,8 +79,7 @@ public class userDB {
         byte[] hashedPassword = md.digest(password.getBytes(StandardCharsets.UTF_8));
 
         // This is the way a password should be encoded when checking the credentials
-        return new String(hashedPassword, StandardCharsets.UTF_8)
-                .replace("\"", ""); //to be able to save in JSON format
+        return new String(hashedPassword, StandardCharsets.UTF_8);
     }
 
     public static MessageDigest getMessageDigest() {
